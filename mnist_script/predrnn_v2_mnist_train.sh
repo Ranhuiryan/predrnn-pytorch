1ea858c8d45d04d1b270a165c7bcb9ef5693e518
Ranhuiryan <qdgjhrh@outlook.com>
2022-07-02 10:54:59 +0800

    add fracture data

--- mnist_script/predrnn_v2_mnist_train.sh
+++ mnist_script/predrnn_v2_mnist_train.sh
@@ -1,19 +1,23 @@
+IMGTYPE=skel
+INPUTLENGTH=4
+TOTALLENGTH=5
+
 export CUDA_VISIBLE_DEVICES=0
 cd ..
 python -u run.py \
     --is_training 1 \
     --device cuda \
     --dataset_name mnist \
-    --train_data_paths /workspace/wuhaixu/predrnn/data/moving-mnist-example/moving-mnist-train.npz \
-    --valid_data_paths /workspace/wuhaixu/predrnn/data/moving-mnist-example/moving-mnist-valid.npz \
-    --save_dir checkpoints/mnist_predrnn_v2 \
-    --gen_frm_dir results/mnist_predrnn_v2 \
+    --train_data_paths ./data/moving-mnist-example/fracture-$IMGTYPE-train-input-$INPUTLENGTH-seq-$TOTALLENGTH.npz \
+    --valid_data_paths ./data/moving-mnist-example/fracture-$IMGTYPE-valid-input-$INPUTLENGTH-seq-$TOTALLENGTH.npz \
+    --save_dir checkpoints/fracture-$IMGTYPE-input-$INPUTLENGTH-seq-$TOTALLENGTH \
+    --gen_frm_dir results/fracture-$IMGTYPE-input-$INPUTLENGTH-seq-$TOTALLENGTH \
     --model_name predrnn_v2 \
     --reverse_input 1 \
-    --img_width 64 \
+    --img_width 100 \
     --img_channel 1 \
-    --input_length 10 \
-    --total_length 20 \
+    --input_length $INPUTLENGTH \
+    --total_length $TOTALLENGTH \
     --num_hidden 128,128,128,128 \
     --filter_size 5 \
     --stride 1 \
@@ -25,9 +29,9 @@ python -u run.py \
     --r_sampling_step_2 50000 \
     --r_exp_alpha 2500 \
     --lr 0.0001 \
-    --batch_size 8 \
-    --max_iterations 80000 \
+    --batch_size 4 \
+    --max_iterations 10000 \
     --display_interval 100 \
-    --test_interval 5000 \
-    --snapshot_interval 5000 \
+    --test_interval 2000 \
+    --snapshot_interval 2000 \
 #    --pretrained_model ./checkpoints/mnist_predrnn_v2/mnist_model.ckpt
